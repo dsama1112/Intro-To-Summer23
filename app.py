29cb39169aebccc00f233e4b0f17b3772a163dca
Sama <dsama1112dar@gmail.com>
2024-07-17 10:52:40 +0300

    finished button bg color

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ def home():
     return ('''<html>
     <h1>Hello, here's pictures of food, an animal and outer space</h1> 
 
-    <img wdith="500px" src="https://thecozycook.com/wp-content/uploads/2022/04/Lasagna-Recipe-f.jpg"></html>''')
+    <img width="500px" src="https://thecozycook.com/wp-content/uploads/2022/04/Lasagna-Recipe-f.jpg">
+    <img width="500px" src="https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg">
+    <img width="500px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Webb%27s_First_Deep_Field.jpg/310px-Webb%27s_First_Deep_Field.jpg">
+
+
+    </html>''')
 
 
 
